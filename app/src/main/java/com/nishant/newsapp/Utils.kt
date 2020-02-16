79491139a0f308528734d92e41a38e105311d86b
Nishant Pardamwar <nishantpardamwar@gmail.com>
2020-02-16 12:29:52 +0530

    open web link on news item click

--- app/src/main/java/com/nishant/newsapp/Utils.kt
+++ app/src/main/java/com/nishant/newsapp/Utils.kt
@@ -1,6 +1,8 @@
 package com.nishant.newsapp
 
 import android.content.res.Resources
+import java.text.SimpleDateFormat
+import java.util.*
 
 fun dpToPx(dp: Float): Float {
     return dp * Resources.getSystem().displayMetrics.density
@@ -8,4 +10,18 @@ fun dpToPx(dp: Float): Float {
 
 fun pxToDp(px: Float): Float {
     return px / Resources.getSystem().displayMetrics.density
+}
+
+fun getDisplayDate(dateString: String?): String? {
+    try {
+        dateString?.let {
+            val inputFormat = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'", Locale.getDefault())
+            val formatter = SimpleDateFormat("dd MMM yyyy HH:mm", Locale.getDefault())
+            inputFormat.parse(dateString)?.let { date ->
+                return formatter.format(date)
+            }
+        }
+    } catch (e: Exception) {
+    }
+    return null
 }
--- app/src/main/java/com/nishant/newsapp/home/NewsAdapter.kt
+++ app/src/main/java/com/nishant/newsapp/home/NewsAdapter.kt
@@ -1,12 +1,16 @@
 package com.nishant.newsapp.home
 
+import android.content.Intent
+import android.net.Uri
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.nishant.newsapp.R
+import com.nishant.newsapp.getDisplayDate
 import com.nishant.newsapp.model.Articles
+import com.nishant.newsapp.nonEmptyStringOrNull
 import kotlinx.android.synthetic.main.vh_article.view.*
 
 class NewsAdapter(private var list: List<Articles>) :
@@ -33,11 +37,22 @@ class NewsAdapter(private var list: List<Articles>) :
     }
 
     inner class ViewHolder(val view: View) : RecyclerView.ViewHolder(view) {
+        init {
+            view.setOnClickListener {
+                list.getOrNull(adapterPosition)?.let { article ->
+                    val intent = Intent(Intent.ACTION_VIEW, Uri.parse(article.url))
+                    it.context.startActivity(intent)
+                }
+            }
+        }
+
         fun bind(position: Int) {
             list.getOrNull(position)?.let { article ->
                 view.txt_title.text = article.title
                 view.txt_author.text = article.author
                 view.txt_description.text = article.description
+                view.txt_publish_date.text =
+                    getDisplayDate(article.publishedAt).nonEmptyStringOrNull() ?: ""
 
                 Glide.with(view.image).load(article.urlToImage).into(view.image)
             }
